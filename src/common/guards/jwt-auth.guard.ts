2fdb61d7c2ca738b8052b111e3e6a685478be519
Andresflorezdev <florezandresfelipe62@gmail.com>
2026-05-18 18:11:51 -0500

    fix(auth): personalizar mensaje cuando el token falta o es invalido

--- src/common/guards/jwt-auth.guard.ts
+++ src/common/guards/jwt-auth.guard.ts
@@ -1,4 +1,8 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { AuthGuard } from '@nestjs/passport';
 import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
@@ -20,5 +24,13 @@ export class JwtAuthGuard extends AuthGuard('jwt') {
     }
 
     return super.canActivate(context);
+  }
+
+  handleRequest<TUser = unknown>(err: unknown, user: TUser): TUser {
+    if (err || !user) {
+      throw new UnauthorizedException('Token invalido o ausente');
+    }
+
+    return user;
   }
 }
